f7caceb5da38cf3858d6d96d5c02e0452157c754
YuyaKurodou <kuroyuya0524@gmail.com>
2020-07-12 21:53:26 +0900

    受信側の操作

--- recieve2.py
+++ recieve2.py
@@ -1,5 +1,3 @@
-import math
-
 chara = ["_", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 print("crypt:",end="")  #暗号文を入力
