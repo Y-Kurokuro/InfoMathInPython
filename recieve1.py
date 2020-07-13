3bbb7ee017fecfa93be5cb98865b4bb27503e32a
YuyaKurodou <kuroyuya0524@gmail.com>
2020-07-13 23:21:53 +0900

    受信側の準備

--- recieve1.py
+++ recieve1.py
@@ -1,20 +1,24 @@
 import random
+import math
 
 def prime(n):   #素数判定関数をつくる
-    i = 2
-    while i < n:
+    if n % 2 == 0:
+        return False
+    r = int(math.sqrt(n))
+    i = 3
+    while i < r:
         if n % i == 0:
             break
-        i += 1
-    if i == n:
+        i += 2
+    if i > r:
         return True
     else:
         return False
 
 p, q = 0, 0
 while (p == q) or not prime(p) or not prime(q):   #p == q または　p が素数でない　または　q が素数でない間続ける (p != q かつ p 素数 q 素数 ならば抜ける)
-    p = random.randint(10**4, 10**5)
-    q = random.randint(10**4, 10**5)
+    p = random.randint(10**10, 10**11)
+    q = random.randint(10**10, 10**11)
 #p,qは互いに素な素数なので、最大公約数は1
 
 n = p * q
