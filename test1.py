e5ddf63890ffcf6f78b9c912bb28e11561e644a2
YuyaKurodou <kuroyuya0524@gmail.com>
2020-07-25 17:26:23 +0900

    中間テスト

--- test1.py
+++ test1.py
@@ -15,10 +15,10 @@ for i in range(2):
     for j in range(1):
         b[i][j] = int(input())
 
-det = (a[0][0] * a[1][1] - a[0][1] * a[1][0]) % m
+det = (a[0][0] * a[1][1] - a[0][1] * a[1][0]) % m   #行列式を計算
 
 c = 1
-while c < m:
+while c < m:    #ac≡1(mod m)となるcを見つける
     if (c * det) % m == 1:
         break
     c += 1
@@ -29,13 +29,13 @@ d[0][1] = -a[0][1]
 d[1][0] = -a[1][0]
 d[1][1] = a[0][0]
 
-inv = np.zeros((2,2))
+inv = np.zeros((2,2))   #逆行列invを計算
 for i in range(2):
     for j in range(2):
         inv[i][j] = (d[i][j] * c) % m
 
 
-xy = np.zeros((2,1))
+xy = np.zeros((2,1))    #答えを計算
 xy[0][0] = (inv[0][0] * b[0][0] + inv[0][1] * b[1][0]) % m
 xy[1][0] = (inv[1][0] * b[0][0] + inv[1][1] * b[1][0]) % m
 
